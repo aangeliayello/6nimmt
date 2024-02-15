cf2563ef27976c51e46316249ecd1386caf15dd3
aangeliayello <aloangeli@gmail.com>
2024-02-15 10:31:12 +0100

    Deleted VS Code, build, and other polluting file

--- ui/build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.obj: \
- C:\Users\franc\Documents\_FunAndProfit\6nimmt\ui\build\_deps\sfml-src\src\SFML\Graphics\BlendMode.cpp \
- C:/Users/franc/Documents/_FunAndProfit/6nimmt/ui/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
- C:/Users/franc/Documents/_FunAndProfit/6nimmt/ui/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- C:/Users/franc/Documents/_FunAndProfit/6nimmt/ui/build/_deps/sfml-src/include/SFML/Config.hpp
